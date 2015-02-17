80f9ff89264527bfcca72c5317ccdf6384b46e4d
Mark Macdonald <mark.t.macdonald@googlemail.com>
2015-02-17 17:13:21 +0100

    basic structure for reading yaml with js-yaml

--- DocGen/Generator/docgen.js
+++ DocGen/Generator/docgen.js
@@ -9,6 +9,33 @@ var cheerio = require('cheerio');
 
 function DocGen() {
 
+	this.settings = {
+		output_path: '../output',
+  		output_filename: 'documentation',
+  		output_dirname: 'Docs',
+  		homepage: 'index.html',
+  		suppress_IE_warning: false,
+  		generate_page_TOC: false
+	}
+
+	this.documentSettings = {
+		name: 'Empty Template',
+		owner: 'Department',
+		author: '',
+		email: '',
+		module: '',
+		module_id: '',
+		release: '',
+		summary: 'This is the empty template for DocGen.\n',
+		company: 'Company Name',
+		protective_marking: 'Document protective marking',
+		legalese: 'Copyright notices. \n'
+	}
+
+	this.table_of_contents = {
+
+	}
+
 	this.loadTemplate = function () {
 		fs.readFile('Resources/template.html', 'utf8', function (err, template) {
 		  if (err) {
@@ -19,11 +46,26 @@ function DocGen() {
 		});
 	};
 
+	this.loadConfig = function () {
+		try {
+		  var doc = yaml.safeLoad(fs.readFileSync('sample_document.yml', 'utf8'));
+		  console.log(doc);
+		} catch (e) {
+		  console.log(e);
+		}
+	}
+
+	this.writeFiles = function () {
+		
+	}
+
 	this.run = function () {
 		this.loadTemplate();
+		this.loadConfig();
+		this.writeFiles();
 	}
 
 };
 
 var generator = new DocGen();
-generator.loadTemplate();
+generator.run();
