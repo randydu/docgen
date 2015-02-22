0abd3f3446341d65b6bb0e8c0aba32b772827e4c
Mark Macdonald <mark.t.macdonald@googlemail.com>
2015-02-22 19:34:12 +0100

    basic promise-based markdown loader.

--- DocGen/Generator/tool/docgen.js
+++ DocGen/Generator/tool/docgen.js
@@ -13,84 +13,101 @@ var child_process = require("child_process");
 function DocGen (options)
 {
 
-	var options = options;
-	var parameters;
-	var tableOfContents;
+    var options = options;
+    var parameters;
+    var tableOfContents;
 
-	this.pages = [];
+    this.pages = [];
 
-	this.loadTemplate = function () {
-		fs.readFile('Resources/template.html', 'utf8', function (err, template) {
-		  if (err) {
-		    return console.log(err);
-		  }
-		  $ = cheerio.load(template);
-		  console.log($('title').text());
-		});
-	};
+    this.loadTemplate = function () {
+        fs.readFile('Resources/template.html', 'utf8', function (err, template) {
+          if (err) {
+            return console.log(err);
+          }
+          $ = cheerio.load(template);
+          console.log($('title').text());
+        });
+    };
 
-	this.loadPages = function () {
+    this.writeFiles = function () {
 
-		this.table_of_contents.forEach( function (section) {
-			section.links.forEach( function (page) {
-				
-				fs.readFile(page.url, {encoding: 'utf-8'}, function (err, data) {
-				    if (!err) {
-				    	console.log(marked(data));
-				    } else {
-				        console.log(err);
-				    }
-				});
+    }
 
-			}, this);
-		}, this);
+    this.callExternal = function () {
+        var child = child_process.exec('ls', function (error, stdout, stderr) {
+            console.log(stdout);
+        });
+    }
 
-	}
+    var loadJSON = function (path) {
+        return new rsvp.Promise(function (resolve, reject) {
+            fs.readFile (path, 'utf8', function (error, data) {
+                if (error) {
+                    reject(error);
+                }
+                try{
+                    //todo - validate json against a schema
+                    var result = JSON.parse(data);
+                    resolve(result);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        });
+    };
 
-	this.writeFiles = function () {
+    var loadMarkdown = function (path) {
+        return new rsvp.Promise(function (resolve, reject) {
+            fs.readFile (path, 'utf8', function (error, data) {
+                if (error) {
+                    reject(error);
+                }
+                try{
+                    var result = marked(data);
+                    resolve(result);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        });
 
-	}
+/*
+        this.table_of_contents.forEach( function (section) {
+            section.links.forEach( function (page) {
+                
+                fs.readFile(page.url, {encoding: 'utf-8'}, function (err, data) {
+                    if (!err) {
+                        console.log(marked(data));
+                    } else {
+                        console.log(err);
+                    }
+                });
 
-	this.callExternal = function () {
-		var child = child_process.exec('ls', function (error, stdout, stderr) {
-			console.log(stdout);
-		});
-	}
+            }, this);
+        }, this);
+*/
+    }
 
-	var loadJSON = function (path) {
-		return new rsvp.Promise(function (resolve, reject) {
-			fs.readFile (path, 'utf8', function (error, data) {
-	   			if (error) {
-		      		reject(error);
-	    		}
-			    try{
-			    	//todo - validate json against a schema
-			        var result = JSON.parse(data);
-			        resolve(result);
-			    } catch (error) {
-			    	reject(error);
-			    }
-			});
-		});
-	};
+    this.run = function () {
 
-	this.run = function () {
+        //this.loadTemplate();
+        //this.loadConfig();
+        //this.loadPages();
+        //this.callExternal();
+        //this.writeFiles();
 
-		//this.loadTemplate();
-		//this.loadConfig();
-		//this.loadPages();
-		//this.callExternal();
-		//this.writeFiles();
-
-		loadJSON('src/parameters.json').then(function (parameters) {
-			console.log(parameters);
-		  	return loadJSON('src/index.txt');
-		}).then(function (contents) {
-			console.log(contents);
-		}).catch(function (error) {
-			console.log(error);
-		});
-	}
+        loadJSON('src/parameters.json').then(function (parameters) {
+            console.log(parameters);
+            return loadJSON('src/contents.json');
+        }).then(function (contents) {
+            console.log(contents);
+            return loadMarkdown('src/index.txt');
+        }).then(function (markdown) {
+            console.log(markdown);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
 }
 
 module.exports = DocGen;
