2c36316bdb5855167e7591c70a4715863ba1f6ac
Mark Macdonald <mark.t.macdonald@googlemail.com>
2015-02-24 20:42:27 +0100

    refactor, and now loads all templates, all JSON files, and all markdown files.

--- DocGen/Generator/tool/docgen.js
+++ DocGen/Generator/tool/docgen.js
@@ -3,7 +3,6 @@ var rsvp = require('rsvp');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var marked = require('marked');
-
 var child_process = require("child_process");
 
 /**
@@ -12,24 +11,10 @@ var child_process = require("child_process");
 
 function DocGen (options)
 {
-
     var options = options;
-    var parameters;
-    var tableOfContents;
-
-    this.pages = [];
-
-    this.loadTemplate = function () {
-        fs.readFile('Resources/template.html', 'utf8', function (err, template) {
-          if (err) {
-            return console.log(err);
-          }
-          $ = cheerio.load(template);
-          console.log($('title').text());
-        });
-    };
-
-
+    var templates = {};
+    var meta = {};
+    var pages = {};
 
     this.callExternal = function () {
         var child = child_process.exec('ls', function (error, stdout, stderr) {
@@ -37,32 +22,18 @@ function DocGen (options)
         });
     }
 
-    var loadJSON = function (path) {
-        return new rsvp.Promise(function (resolve, reject) {
-            fs.readFile (path, 'utf8', function (error, data) {
-                if (error) {
-                    reject(error);
-                }
-                try{
-                    //todo - validate json against a schema
-                    var result = JSON.parse(data);
-                    resolve(result);
-                } catch (error) {
-                    reject(error);
-                }
-            });
-        });
-    };
+    /*
+        read any file
+    */
 
-    var loadMarkdown = function (path) {
+    var readFile = function (path) {
         return new rsvp.Promise(function (resolve, reject) {
             fs.readFile (path, 'utf8', function (error, data) {
                 if (error) {
                     reject(error);
                 }
                 try{
-                    var result = marked(data);
-                    resolve(result);
+                    resolve(data);
                 } catch (error) {
                     reject(error);
                 }
@@ -70,43 +41,87 @@ function DocGen (options)
         });
     }
 
-    var writeFiles = function () {
-        return 'done';
+    /*
+        load all HTML template files
+    */
+
+    var loadTemplates = function () {
+        var files = {
+            main: readFile('Resources/template.html'),
+        };
+        rsvp.hash(files).then(function(files) {
+            for (var key in files) {
+                if (files.hasOwnProperty(key)) { //ignore prototype
+                    var dom = cheerio.load(files[key]);
+                    templates[key] = dom;
+                }
+            }
+            loadMeta();
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    /*
+        load all metadata files (JSON)
+    */
+
+    var loadMeta = function () {
+        var files = {
+            parameters: readFile('src/parameters.json'),
+            contents: readFile('src/contents.json'),
+        };
+        rsvp.hash(files).then(function(files) {
+            for(var key in files) {
+                if (files.hasOwnProperty(key)) { //ignore prototype
+                    try{
+                        //todo - validate json against a schema
+                        var result = JSON.parse(files[key]);
+                        meta[key] = result;
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+            }
+            loadMarkdown();
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    /*
+        load all markdown files (source)
+    */
+
+    var loadMarkdown = function () {
+        var keys = [];
+        var files = [];
+        meta.contents.forEach( function (section) {
+            section.links.forEach( function (page) {
+                keys.push(page.src);
+                files.push('src/'+page.src);
+                //var name = page.src.substr(0, page.src.lastIndexOf('.'));
+                //outputs.push('out/'+name+'.html');
+            });
+        });
+        rsvp.all(files.map(readFile)).then(function (files) {
+            files.forEach( function (page, index) {
+                try{
+                    var html = marked(page);
+                    var key = keys[index];
+                    pages[key] = html;
+                } catch (error) {
+                    console.log(error);
+                }
+            });
+            console.log(pages); 
+        }).catch(function(error) {
+            console.log(error);
+        });
     }
 
     this.run = function () {
-
-        //this.loadTemplate();
-        //this.loadConfig();
-        //this.loadPages();
-        //this.callExternal();
-        //this.writeFiles();
-
-        loadJSON('src/parameters.json').then(function (parameters) {
-            console.log(parameters);
-            return loadJSON('src/contents.json');
-        }).then(function (contents) {
-            console.log(contents);
-
-            var files = [];
-            contents.forEach( function (section) {
-                section.links.forEach( function (page) {
-                    files.push('src/'+page.src);
-                });
-            });
-
-            rsvp.all(files.map(loadMarkdown)).then(function(files) {
-              console.log(files);
-              return writeFiles();
-            }).catch(function(error) {
-              console.log(error);
-            });
-
-        }).then(function (markdown) {
-            console.log(markdown);
-        }).catch(function (error) {
-            console.log(error);
-        });
+        loadTemplates();
     }
 }
 
